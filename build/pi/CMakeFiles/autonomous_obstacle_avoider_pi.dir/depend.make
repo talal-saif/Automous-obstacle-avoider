17b2fffca4148a16c33a2c1f33f4422676aa8b3a
talalrobot <talal.robotics@gmail.com>
2025-10-04 14:52:42 +0300

    Add proper .gitignore and clean build/cache

--- build/pi/CMakeFiles/autonomous_obstacle_avoider_pi.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for autonomous_obstacle_avoider_pi.
-# This may be replaced when dependencies are built.
--- build/sim/CMakeFiles/autonomous_obstacle_avoider_sim.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for autonomous_obstacle_avoider_sim.
